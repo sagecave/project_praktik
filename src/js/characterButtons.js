0acb819094ed8c2830a1d6cde5a0db091bb3f2bd
”sagecave” <Andreassage@gmail.com>
2024-09-29 13:58:58 +0200

    næsten done

---
+++ src/js/characterButtons.js
@@ -0,0 +1,9 @@
+const first_ch = document.querySelector(".charcterFirst");
+const secound_ch = document.querySelector(".charcterSecond");
+const third_ch = document.querySelector(".charcterThird");
+
+first_ch.addEventListener("click", hello());
+
+function hello() {
+  console.log("hello");
+}
